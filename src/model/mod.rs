8d0bee36bcac3b32053939ff1d7969abbbc2d3bf
Yasushi Sakai <yasushisakai+git@pm.me>
2021-01-30 19:11:25 -0500

    refoctoring model.rs to it's own directory

---
+++ src/model/mod.rs
@@ -0,0 +1,43 @@
+mod user;
+mod topic;
+
+use liq::Setting;
+use serde::Serialize;
+use std::fmt::Debug;
+
+pub use user::{User, PartialUser};
+pub use topic::{Topic, PartialTopic};
+
+pub trait Settable: Serialize + Debug {
+    fn domain_prefix() -> String;
+
+    fn prefix(&self) -> String {
+        Self::domain_prefix()
+    } //FIXME: theres got be a smarter way...
+
+    fn id(&self) -> String;
+    fn list_item(&self) -> String;
+
+    fn domain(&self) -> String {
+        return format!("{}:{}", Self::domain_prefix(), &self.id());
+    }
+
+    fn json(&self) -> String {
+        serde_json::to_string(&self).expect("I should be Serialize-able")
+    }
+}
+
+impl Settable for Setting {
+    fn domain_prefix() -> String {
+        String::from("setting")
+    }
+    fn id(&self) -> String {
+        self.based_hash()
+    }
+
+    fn list_item(&self) -> String {
+        self.id()
+    }
+}
+
+
--- src/model/topic.rs
+++ src/model/topic.rs
@@ -2,40 +2,9 @@ use bs58::encode;
 use liq::{Plan, PollResult, Setting};
 use serde::{Deserialize, Serialize};
 use sha2::{Digest, Sha256};
-use std::collections::{BTreeMap, HashSet};
+use std::collections::BTreeMap;
 use std::fmt::Debug;
-
-pub trait Settable: Serialize + Debug {
-    fn domain_prefix() -> String;
-
-    fn prefix(&self) -> String {
-        Self::domain_prefix()
-    } //FIXME: theres got be a smarter way...
-
-    fn id(&self) -> String;
-    fn list_item(&self) -> String;
-
-    fn domain(&self) -> String {
-        return format!("{}:{}", Self::domain_prefix(), &self.id());
-    }
-
-    fn json(&self) -> String {
-        serde_json::to_string(&self).expect("I should be Serialize-able")
-    }
-}
-
-impl Settable for Setting {
-    fn domain_prefix() -> String {
-        String::from("setting")
-    }
-    fn id(&self) -> String {
-        self.based_hash()
-    }
-
-    fn list_item(&self) -> String {
-        self.id()
-    }
-}
+use crate::model::Settable;
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Topic {
@@ -69,10 +38,6 @@ impl Topic {
 
     pub fn remove_plan(&mut self, text: String) {
         self.setting.delete_plan(&text);
-    }
-
-    pub fn get_users(&self) -> HashSet<String> {
-        self.setting.get_voters()
     }
 
     pub fn add_user(&mut self, user_id: String) {
@@ -128,53 +93,4 @@ impl From<PartialTopic> for Topic {
     }
 }
 
-#[derive(Debug, Deserialize, Serialize)]
-pub struct PartialUser {
-    pub nickname: String,
-    pub email: String,
-}
 
-impl From<PartialUser> for User {
-    fn from(p_user: PartialUser) -> User {
-        User::new(p_user.nickname, p_user.email)
-    }
-}
-
-impl Settable for User {
-    fn domain_prefix() -> String {
-        String::from("user")
-    }
-
-    fn id(&self) -> String {
-        self.id.to_string()
-    }
-
-    fn list_item(&self) -> String {
-        serde_json::to_string(&vec![&self.id, &self.nickname])
-            .expect("User-Settable should be Serializable")
-    }
-}
-
-#[derive(Debug, Deserialize, Serialize, Clone)]
-pub struct User {
-    id: String,
-    pub nickname: String,
-    pub is_verified: bool,
-}
-
-impl User {
-    pub fn new(nickname: String, email: String) -> Self {
-        let cat = format!("email:{}", &email);
-        let id = encode(Sha256::digest(&cat.as_bytes())).into_string();
-
-        Self {
-            id,
-            nickname,
-            is_verified: false,
-        }
-    }
-
-    pub fn json(&self) -> String {
-        serde_json::to_string(self).expect("user should be able to serialize")
-    }
-}
